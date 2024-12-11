9b0208d1722a90c93beb0aeb0f878838c29b0366
Harper Foley <harper.e.foley@gmail.com>
2024-12-11 08:27:06 -0800

    Connect header links to appropriate pages. For testing and demo purposes this commit connects the header items to their appropriate links in the app router. Additionally, the VeteranPCS logo now serves as a link to the homepage in line with navigation best practices.
    Comments for DEV team are in the update /blog route:
    - The team will need to ensure that these pages are primarily rendered on the server to take advantage of the SEO benefits of Next.js.
    - These pages must support the Metadata object from Next as well as JSON-LD, which requires server-side rendering.
    - Sanity allows us to take advantage of this by conducting server side API calls to our CMS and rendering the HTML at build time.
    - Blogs should use design patterns that allow the majority of content to be rendered on the server.
    - Aspects that require client interactivity should be moved to their own components or pulled in using dynamic from next/dynamic.

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,15 +8,15 @@ const Header = () => {
     <div className="bg-[#292f6c] sticky top-0 left-0 w-full z-50 header px-5">
       <div className="container mx-auto">
         <div className="flex items-center justify-between gap-4 ">
-          <div className="py-5">
+          <Link className="py-5" href="/">
             <Image
               width={235}
               height={63}
               src="/icon/VeteranPCSlogo.svg"
               className="lg:w-[235px] lg:h-[63px] md:w-[235px] md:h-[63px] sm:w-[200px] sm:h-[63px] w-[200px] h-[63px]"
-              alt="Description of the image"
+              alt="VeteranPCS logo"
             />
-          </div>
+          </Link>
           <div className="flex items-center gap-4">
             <div className="py-5 mr-10">
               <nav className="menu-nav">
@@ -31,26 +31,26 @@ const Header = () => {
                     />
                   </button>
                 </div>
-                <ul className="menu nav flex items-center lg:gap-20 md:gap-10 sm:gap-5 gap-5 ml-5 lg:flex md:flex sm:hidden hidden">
+                <ul className="menu nav items-center lg:gap-20 md:gap-10 sm:gap-5 gap-5 ml-5 lg:flex md:flex sm:hidden hidden">
                   <li>
-                    <Link className="text-base font-normal text-white" href="#">
+                    <Link className="text-base font-normal text-white" href="/about">
                       About
                     </Link>
                     <ul className="sub-menu bg-[#292E6C] border-t-2 border-[#003486] p-5">
                       <li className="px-10 py-3 text-white">
-                        <Link className="text-base font-normal" href="#">
+                        <Link className="text-base font-normal" href="/how-it-works">
                           How It Works
                         </Link>
                       </li>
                       <li className="px-10 py-3 text-white">
-                        <Link className="text-base font-normal" href="#">
+                        <Link className="text-base font-normal" href="/stories">
                           Stories
                         </Link>
                       </li>
                       <li className="px-10 py-3 text-white">
                         <Link
                           className="text-base font-normal"
-                          href="/blog-list"
+                          href="/blog"
                         >
                           Blog
                         </Link>
@@ -58,17 +58,17 @@ const Header = () => {
                     </ul>
                   </li>
                   <li>
-                    <Link className="text-base font-normal text-white" href="#">
+                    <Link className="text-base font-normal text-white" href="/pcs-resources">
                       PCS Resources
                     </Link>
                   </li>
                   <li>
-                    <Link className="text-base font-normal text-white" href="#">
+                    <Link className="text-base font-normal text-white" href="/impact">
                       Impact
                     </Link>
                   </li>
                   <li>
-                    <Link className="text-base font-normal text-white" href="#">
+                    <Link className="text-base font-normal text-white" href="/contact">
                       Contact
                     </Link>
                     <ul className="sub-menu">
@@ -91,7 +91,7 @@ const Header = () => {
               <div className="text-center py-[28px]">
                 <p className="text-white text-[33px]">
                   <strong className="text-[33px] text-white font-bold">
-                    $247,500<strong></strong>
+                    $247,500
                   </strong>
                 </p>
                 <p className="py-4 text-white mb-0 pb-0 text-xs">
