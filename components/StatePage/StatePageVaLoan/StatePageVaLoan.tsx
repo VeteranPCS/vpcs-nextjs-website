d3d6a651ed271b91ca12d2ec5c35f7b3650e22bd
VPCS-Tech <tech@veteranpcs.com>
2025-11-12 14:49:09 -0800

    Fixes for agent bio expand/collapse when agent is in multiple cities on state page

--- components/StatePage/StatePageVaLoan/StatePageVaLoan.tsx
+++ components/StatePage/StatePageVaLoan/StatePageVaLoan.tsx
@@ -65,16 +65,15 @@ const StatePageVaLoan = ({ cityName, lendersData, state }: { cityName: string, l
                     </div>
                     <div className="relative">
                       {/* Hidden checkbox to track toggle state */}
-                      <input type="checkbox" id={`toggle-${index + lender.Name}`} className="peer hidden" />
+                      <input type="checkbox" id={`toggle-${index + ' ' + lender.Name + ' ' + cityName.toLowerCase().split(' ').join('-')}`} className="peer hidden" />
 
                       {/* Text that expands/collapses */}
-                      <p className="text-[#747D88] tahoma lg:text-[18px] md:text-[18px] sm:text-[14px] text-[14px] font-normal mt-4 
-      max-h-[80px] overflow-hidden peer-checked:max-h-full transition-all duration-300">
+                      <p className="text-[#747D88] tahoma lg:text-[18px] md:text-[18px] sm:text-[14px] text-[14px] font-normal mt-4 max-h-[80px] overflow-hidden peer-checked:max-h-full transition-all duration-300">
                         {lender?.Agent_Bio__pc}
                       </p>
 
                       {/* Single label that toggles state */}
-                      <label htmlFor={`toggle-${index + lender.Name}`} className="cursor-pointer text-[#292F6C] tahoma text-sm font-bold mt-2 block peer-checked:before:content-['Read_Less'] before:content-['...Read_More']">
+                      <label htmlFor={`toggle-${index + ' ' + lender.Name + ' ' + cityName.toLowerCase().split(' ').join('-')}`} className="cursor-pointer text-[#292F6C] tahoma text-sm font-bold mt-2 block peer-checked:before:content-['Read_Less'] before:content-['...Read_More']">
                       </label>
                     </div>
 
--- components/StatePage/StatePaheHeroSection/CitySelection.tsx
+++ components/StatePage/StatePaheHeroSection/CitySelection.tsx
@@ -8,7 +8,9 @@ interface CitySelectionProps {
 
 const CitySelection = ({ cityList }: CitySelectionProps) => {
     return (
-        <select className="w-full max-w-xs px-4 py-2 bg-white border h-[45px] rounded-md shadow-sm focus:outline-none focus:ring-2 mb-2 focus:ring-[#ffffff] focus:border-[#ffffff]"
+        <select
+            className="w-full max-w-xs px-4 py-2 bg-white border h-[45px] rounded-md shadow-sm focus:outline-none focus:ring-2 mb-2 focus:ring-[#ffffff] focus:border-[#ffffff]"
+            aria-label="Select an Area"
             onChange={(city) => {
                 const targetElement = document.getElementById(sanitizeCityName(city.target.value));
                 if (targetElement) {
